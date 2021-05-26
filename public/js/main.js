910087ce45a0f83eebb9241481bfdba01857c0a7
dhiraj26.dk <dhiraj26.dk@gmail.com>
2021-05-26 20:53:16 +0530

    editing first website

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,7 @@ const func1=async(event)=>{
     }
     else{
         try {
-            let url=`http://api.openweathermap.org/data/2.5/find?q=${cname}&units=metric&appid=13ca4d63f152b5a02317bb23d8fbaf01`;
+            let url=`https://api.openweathermap.org/data/2.5/find?q=${cname}&units=metric&appid=13ca4d63f152b5a02317bb23d8fbaf01`;
             const response=await fetch(url);
             const data=await response.json();
             const arrdata=[data];
